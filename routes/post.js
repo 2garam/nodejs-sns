76dec3a89f33793a4e93e60cb59ae1a9f7225838
byunguk2 <foryou0129@naver.com>
2020-04-04 16:06:26 +0900

    img upload & hashtag search

--- routes/post.js
+++ routes/post.js
@@ -2,13 +2,16 @@ const express = require("express");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
-// const { Post, Hashtag, User } = require("../models");
-const { Post, User } = require("../models");
+
+const { Post, Hashtag, User } = require("../models");
 const { isLoggedIn } = require("./middlewares");
+
 const router = express.Router();
+
 fs.readdir("uploads", error => {
   if (error) {
     console.error("uploads 폴더가 없어 uploads 폴더를 생성합니다.");
+    fs.mkdirSync("uploads");
   }
 });
 const upload = multer({
@@ -38,23 +41,47 @@ router.post("/", isLoggedIn, upload2.none(), async (req, res, next) => {
       img: req.body.url,
       userId: req.user.id
     });
-    // const hashtags = req.body.content.match(/#[^\s]*/g);
-    // if (hashtags) {
-    //   const result = await Promise.all(
-    //     hashtags.map(tag =>
-    //       Hashtag.findOrCreate({
-    //         where: {
-    //           title: tag.slice(1).toLowerCase()
-    //         }
-    //       })
-    //     )
-    //   );
-    //   await post.addHashtags(result.map(r => r[0]));
-    // }
+    const hashtags = req.body.content.match(/#[^\s]*/g);
+    if (hashtags) {
+      const result = await Promise.all(
+        hashtags.map(tag =>
+          Hashtag.findOrCreate({
+            where: {
+              title: tag.slice(1).toLowerCase()
+            }
+          })
+        )
+      );
+      await post.addHashtags(result.map(r => r[0]));
+    }
     res.redirect("/");
   } catch (error) {
     console.error(error);
     next(error);
   }
 });
+
+router.get("/hashtag", async(req, res, next) => {
+  const query = req.query.hashtag;
+  if(!query) {
+    return res.redirect('/');
+  }
+  try {
+    const hashtag = await Hashtag.findOne({ where: { title: query }});
+    let posts = [];
+    if (hashtag) {
+      posts = await hashtag.getPosts({ include: [{ model: User}] });
+      return res.render("main", {
+        title: `${query} | NodeBird`,
+        user: req.user,
+        twits: posts
+      });
+    }
+  }
+  catch(error) {
+    console.error(error);
+    return next(error);
+  }
+});
+
 module.exports = router;
